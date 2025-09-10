b0da5c37937cbcb10155ae2849cfa09e537b6a25
Saif <saif2107017@gmail.com>
2025-09-10 22:11:18 +0600

    final touch completed

--- Dashboard.aspx.cs
+++
@@ -1,1 +0,0 @@
-﻿using System; using System.Web.UI; namespace SaifPortFoliio { public partial class Dashboard : Page { protected void Page_Load(object sender, EventArgs e) { Response.Redirect("Contact.aspx"); } } }
